9a9a545af5298adbc57c25394f88d5159de6baf9
Reece <reece.chawda@outlook.com>
2023-11-21 19:17:50 +0000

    Implemented Hive Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:notepad/note.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() => runApp(const NoteApp());
+import 'note.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(NoteAdapter());
+  runApp(const NoteApp());
+}
 
 class NoteApp extends StatelessWidget {
   const NoteApp({Key? key});
@@ -22,11 +28,31 @@ class NotePadApp extends StatefulWidget {
 }
 
 class NotePadAppState extends State<NotePadApp> {
+  late Box<Note> noteBox;
   List<Note> notes = [];
   TextEditingController noteInput = TextEditingController();
   TextEditingController titleInput = TextEditingController();
 
-  void noteBox() {
+  void updateNoteList(Function(List<Note>) updateFunction) {
+    setState(() {
+      updateFunction(notes);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    openBox();
+  }
+
+  Future<void> openBox() async {
+    noteBox = await Hive.openBox<Note>('notes');
+    setState(() {
+      notes = noteBox.values.toList();
+    });
+  }
+
+  void noteDialog() {
     showDialog(
       context: context,
       builder: (context) {
@@ -34,9 +60,9 @@ class NotePadAppState extends State<NotePadApp> {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.0),
           ),
-          backgroundColor: Colors.grey[900], // Dark background color
+          backgroundColor: Colors.grey[900],
           child: Container(
-            width: 600, // Adjust the width as per your preference
+            width: 600,
             padding: const EdgeInsets.all(30.0),
             child: Column(
               mainAxisSize: MainAxisSize.min,
@@ -92,12 +118,14 @@ class NotePadAppState extends State<NotePadApp> {
     );
   }
 
+  // add / update notes
   void addNote() {
     String noteTitle = titleInput.text;
     String noteText = noteInput.text;
     if (noteTitle.isNotEmpty || noteText.isNotEmpty) {
       setState(() {
-        notes.add(Note(noteTitle, noteText));
+        noteBox.add(Note(noteTitle, noteText));
+        notes = noteBox.values.toList();
         clearControllers();
       });
     }
@@ -113,6 +141,7 @@ class NotePadAppState extends State<NotePadApp> {
         TextEditingController(text: note.title);
     TextEditingController editNoteController =
         TextEditingController(text: note.text);
+
 
     showDialog(
       context: context,
@@ -166,6 +195,7 @@ class NotePadAppState extends State<NotePadApp> {
                         setState(() {
                           notes[index].title = editTitleController.text;
                           notes[index].text = editNoteController.text;
+                          noteBox.putAt(index, notes[index]);
                         });
                         Navigator.of(context).pop();
                       },
@@ -182,7 +212,6 @@ class NotePadAppState extends State<NotePadApp> {
 
   @override
   Widget build(BuildContext context) {
-    //sorting system (star button)
     notes.sort((a, b) {
       if (a.isPriority && !b.isPriority) {
         return -1;
@@ -224,20 +253,21 @@ class NotePadAppState extends State<NotePadApp> {
               itemCount: notes.length,
               itemBuilder: (context, index) {
                 return Card(
+                  key: UniqueKey(),
                   elevation: 5,
                   margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
                   color: Colors.grey[800],
                   child: ListTile(
-                    contentPadding: const EdgeInsets.fromLTRB(25, 5, 0 ,10),
+                    contentPadding: const EdgeInsets.fromLTRB(25, 5, 0, 10),
                     title: Padding(
                       padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
                       child: Text(
-                      notes[index].title,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        color: Colors.teal,
+                        notes[index].title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        ),
                       ),
-                    ),
                     ),
                     subtitle: Text(
                       notes[index].text,
@@ -257,6 +287,7 @@ class NotePadAppState extends State<NotePadApp> {
                             setState(() {
                               notes[index].isPriority =
                                   !notes[index].isPriority;
+                              noteBox.putAt(index, notes[index]);
                             });
                           },
                         ),
@@ -269,7 +300,6 @@ class NotePadAppState extends State<NotePadApp> {
                       ],
                     ),
                     onLongPress: () {
-                      // Show a confirmation dialog
                       showDialog(
                         context: context,
                         builder: (context) {
@@ -283,14 +313,14 @@ class NotePadAppState extends State<NotePadApp> {
                               child: Text(
                                 "Are you sure you want to delete this note?",
                                 style: TextStyle(
-                                  color: Colors.grey[300],
+                                  color: Colors.grey[500],
                                   fontSize: 18,
                                 ),
                               ),
                             ),
                             actions: [
                               TextButton(
-                                child: Text(
+                                child: const Text(
                                   "Cancel",
                                   style: TextStyle(color: Colors.teal),
                                 ),
@@ -299,17 +329,16 @@ class NotePadAppState extends State<NotePadApp> {
                                 },
                               ),
                               TextButton(
-                                child: Text(
+                                child: const Text(
                                   "Delete",
                                   style: TextStyle(color: Colors.teal),
                                 ),
                                 onPressed: () {
-                                  // Delete the note
                                   setState(() {
                                     notes.removeAt(index);
+                                    noteBox.deleteAt(index);
                                   });
-                                  Navigator.of(context)
-                                      .pop(); // Close the dialog
+                                  Navigator.of(context).pop();
                                 },
                               ),
                             ],
@@ -326,12 +355,18 @@ class NotePadAppState extends State<NotePadApp> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            buildFloatingActionButton(Icons.note_add_sharp, noteBox),
+            buildFloatingActionButton(Icons.note_add_sharp, noteDialog),
             const SizedBox(width: 10),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    noteBox.close();
+    super.dispose();
   }
 
   Widget buildFloatingActionButton(IconData icon, onPressed) {
--- lib/note.dart
+++ lib/note.dart
@@ -1,8 +1,22 @@
+import 'package:hive/hive.dart';
+
+part 'note.g.dart';
+
+@HiveType(typeId: 0)
 class Note {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String text;
+
+  @HiveField(2)
   bool isPriority;
+
+  @HiveField(3)
   bool isSelected;
 
-  Note(this.title, this.text, {this.isPriority =false, this.isSelected = false});
+  Note(this.title, this.text, {this.isPriority = false, this.isSelected = false});
 }
+
+
